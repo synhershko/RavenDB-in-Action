7c237b7ddb050b1ed97d4e57b35136577223e43a
Itamar Syn-Hershko <itamar@code972.com>
2015-01-13 17:57:55 +0200

    Cleaning up

--- Chapter7.Sharding/Models/Order.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chapter7.Sharding.Models
-{
-    public class Order
-    {
-        public string Id { get; set; }
-        public string UserId { get; set; }
-        public double TotalPrice { get; set; }
-    }
-}
